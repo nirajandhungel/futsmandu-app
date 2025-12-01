ed994ed424acaa4dc2db929ef72f44ba4ff8edb0
nirajandhungel <subashdhungel555@gmail.com>
2025-12-01 13:07:56 +0545

    update edit profile

--- lib/screens/profile/edit_profile.dart
+++ lib/screens/profile/edit_profile.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import '../../providers/auth_provider.dart';
-import '../../models/user.dart';
 import '../../utils/theme.dart';
 import '../../utils/validators.dart';
 import '../../utils/helpers.dart';
